c80ea4668f5d50dd70d408460ae581538b5cd448
Shripal Mehta <shripal.nextstep@gmail.com>
2021-05-10 15:26:11 +0530

    Portfolio

---
+++ 1. Portfolio/script.js
@@ -0,0 +1,7 @@
+function show(choice) {
+    document.getElementById(choice + 'Data').style.display = 'block';
+}
+
+function hide(choice) {
+    document.getElementById(choice + 'Data').style.display = 'none';
+}
